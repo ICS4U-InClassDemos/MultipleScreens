2c3a4a430ec2c056b77757df21673bf2119a1d4e
Tony Theodoropoulos <theodot@outlook.com>
2025-02-05 14:58:47 -0500

    Add project files.

---
+++ MultipleScreens/MainScreen.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MultipleScreens
+{
+    partial class MainScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(117, 75);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(101, 48);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.Enter += new System.EventHandler(this.startButton_Enter);
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Location = new System.Drawing.Point(117, 142);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(101, 48);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.exitButton.Enter += new System.EventHandler(this.exitButton_Enter);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainScreen";
+            this.Size = new System.Drawing.Size(357, 302);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
---
+++ MultipleScreens/MainScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleScreens
+{
+    public partial class MainScreen : UserControl
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+
+            SecondScreen ss = new SecondScreen();
+            ss.Location = new Point((f.Width - ss.Width) / 2, (f.Height - ss.Height) / 2);
+            f.Controls.Add(ss);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void startButton_Enter(object sender, EventArgs e)
+        {
+            startButton.BackColor = Color.Green;
+            exitButton.BackColor = Color.Black;
+        }
+
+        private void exitButton_Enter(object sender, EventArgs e)
+        {
+            startButton.BackColor = Color.Black;
+            exitButton.BackColor = Color.Green;
+        }
+    }
+}
